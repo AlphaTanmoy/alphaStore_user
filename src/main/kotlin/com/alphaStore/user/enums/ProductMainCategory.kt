6800323db7651ce8e5e6b10394e0a2d4ec890d79
alphaTanmoy <dtanmoy169@gmail.com>
2024-12-30 22:32:29 +0530

    enums centralized

--- src/main/kotlin/com/alphaStore/user/enums/ProductMainCategory.kt
+++ src/main/kotlin/com/alphaStore/user/enums/ProductMainCategory.kt
@@ -1,9 +1,10 @@
 package com.alphaStore.user.enums
 
 enum class ProductMainCategory (val nameDescriptor: String)  {
-    TECH("Active"),
-    CLOTHS("Inactive"),
-    FOOD("Food"),
-    DRINKS("Drinks"),
-    OTHERS("Others")
+    DOG("Dog"),
+    CAT("Cat"),
+    BIRD("Bird"),
+    AQUARIUM_AND_POND("Aquarium And Pond"),
+    REPTILE_AND_AMPHIBIAN("Reptile and Amphibian"),
+    Others("Others")
 }
--- src/main/kotlin/com/alphaStore/user/enums/ProductSubCategory.kt
+++ src/main/kotlin/com/alphaStore/user/enums/ProductSubCategory.kt
@@ -1,8 +1,14 @@
 package com.alphaStore.user.enums
 
 enum class ProductSubCategory (val nameDescriptor: String)  {
-    MAN("Man"),
-    WOMAN("Woman"),
-    CHILDREN("Children"),
-    NOT_APPLICABLE("Not Applicable")
+    BEDS("Beds"),
+    CLEAN_UP("Clean Up"),
+    CLOTHING_AND_APPAREL("Clothing & Apparel"),
+    COLLARS_KENNELS_HOUSES("Collars, Kennels & Houses"),
+    FOOD("Food"),
+    DOORS_AND_GATES("Doors & Gates"),
+    FEEDER_AND_WATERERS("Feeder And Waterers"),
+    FLEA_AND_TICK("Flea & Ticks"),
+    HEALTH_CARE("Health Care"),
+    Others("Others")
 }
